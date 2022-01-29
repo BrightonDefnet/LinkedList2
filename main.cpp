05d4d7dc0087af2ba1f34041a34ac79494498508
BrightonDefnet <475373@bsd48.org>
2022-01-29 12:58:19 -0800

    finished add function

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 #include "Student.h"
 using namespace std;
 
-Node* newStudent();
 Node* add(Node* head);
-Node* print(Node* next, Node* head);
+void print(Node* next, Node* head);
 
 int main() {
     Node* head = NULL;
@@ -14,58 +13,60 @@ int main() {
         string result;
         cout << "what would you like to do?\nadd\nquit" << endl;
         cin >> result;
-        result = "add";
 
         if (result == "add") { //add a new student
-            head = add(newStudent());
+            head = add(head);
+            print(head, head);
+        } else if (result == "quit") { //quit the program
+            running = false;
+            break;
+
+        } else { //error handler
+            cout << "not a command" << endl;
         }
-        //} else if (result == "quit") { //quit the program
-            //running = false;
-            //break;
-
-        //} else { //error handler
-            //cout << "not a command" << endl;
-        //}
     }
-}
-
-//create a new student
-Node* newStudent() {
-    Student stu;
-    float gpa;
-    int id;
-    string first;
-    string last;
-    cout << "enter gpa" << endl;
-    cin >> gpa;
-    cout << "enter id" << endl;
-    cin >> id;
-    cout << "enter first name" << endl;
-    cin >> first;
-    cout << "enter last name" << endl;
-    cin >> last;
-    stu.setGpa(gpa);
-    stu.setID(id);
-    stu.setFirst(first);
-    stu.setLast(last);
-    Node head = Node(&stu);
-    Node* ptr = &head;
-    return ptr;
 }
 
 //add a student to the registry
 Node* add(Node* head) {
     Node* current = head;
+        float gpa;
+        int id;
+        string first;
+        string last;
+        cout << "enter gpa" << endl;
+        cin >> gpa;
+        cout << "enter id" << endl;
+        cin >> id;
+        cout << "enter first name" << endl;
+        cin >> first;
+        cout << "enter last name" << endl;
+        cin >> last;
+        Student stu;
+        stu.setFirst(first);
+        stu.setLast(last);
+        stu.setGpa(gpa);
+        stu.setID(id);
     if (current == NULL) {
-        head = newStudent();
+        head = new Node(&stu);
         return head;
     } else {
         while (current->getNext() != NULL) {
             current = current->getNext();
         }
-        current->setNext(newStudent());
+        Node* node = new Node(&stu);
+        current->setNext(node);
         return head;
     }
 }
 
 //print student info
+void print(Node* next, Node* head) {
+    if (next != NULL) {
+        Student* temp = next->getStudent();
+        cout << "\n" << temp->getFirst() << " " << temp->getLast() << endl;
+        cout << "gpa: " << temp->getGpa() << endl;
+        cout << "id: " << temp->getId() << endl;
+        print(next->getNext(), head);
+    }
+}
